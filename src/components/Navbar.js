7726adfe20282d51ef7462d744ef972d4061a7fb
srinivas2422 <152271135+srinivas2422@users.noreply.github.com>
2024-06-22 20:37:52 +0530

    FIRST

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 // import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
-const Navbar = ({ isAuthenticated, handleSignOut}) => {
+const Navbar = ({ isAuthenticated, handleSignOut }) => {
   return (
     <header>
-      <nav className="navbar navbar-expand-md bg-secondary" aria-label="Thirteenth navbar example">
+      <nav
+        className="navbar navbar-expand-md bg-secondary"
+        aria-label="Thirteenth navbar example"
+      >
         <div className="container-fluid">
           <Link className="navbar-brand col-lg-2 me-0" to="/">
             Nex Home
@@ -21,7 +24,10 @@ const Navbar = ({ isAuthenticated, handleSignOut}) => {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
+          <div
+            className="collapse navbar-collapse d-lg-flex"
+            id="navbarsExample11"
+          >
             <ul className="navbar-nav col-lg-6 justify-content-lg-center mx-auto mb-2 mb-lg-0 ">
               <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">
@@ -57,17 +63,57 @@ const Navbar = ({ isAuthenticated, handleSignOut}) => {
             <div className="d-lg-flex col-lg-3 justify-content-lg-end">
               {isAuthenticated ? (
                 <>
-                  <Link className="btn btn-primary mx-1" to="/profile" role="button">
-                    Profile
-                  </Link>
-                  <button className="btn btn-primary mx-1" onClick={handleSignOut}>Sign Out</button>
+                  <div className="flex-shrink-0 dropdown ">
+                    <Link
+                      href="#"
+                      className="d-block link-body-emphasis text-decoration-none dropdown-toggle"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img
+                        src="https://github.com/mdo.png"
+                        alt="mdo"
+                        width={32}
+                        height={32}
+                        className="rounded-circle"
+                      />
+                    </Link>
+                    <ul
+                      className="dropdown-menu text-small shadow dropdown-menu-lg-end"
+                    >
+                      
+                      <li>
+                        <Link className="dropdown-item" href="#">
+                          Profile
+                        </Link>
+                      </li>
+                      
+                      <li>
+                        <Link
+                          className="dropdown-item"
+                          href="#"
+                          onClick={handleSignOut}
+                        >
+                          Sign out
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
                 </>
               ) : (
                 <>
-                  <Link className="btn btn-primary mx-1" to="/signin" role="button">
+                  <Link
+                    className="btn btn-primary mx-1"
+                    to="/signin"
+                    role="button"
+                  >
                     Sign In
                   </Link>
-                  <Link className="btn btn-primary mx-1" to="/signup" role="button">
+                  <Link
+                    className="btn btn-primary mx-1"
+                    to="/signup"
+                    role="button"
+                  >
                     Sign Up
                   </Link>
                 </>
@@ -81,4 +127,3 @@ const Navbar = ({ isAuthenticated, handleSignOut}) => {
 };
 
 export default Navbar;
-
